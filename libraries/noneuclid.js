21a2baee1e4b58661c079a097898b1fa16935def
HanchaiN <85230240+HanchaiN@users.noreply.github.com>
2022-01-03 16:43:43 +0000

    Update libraries

--- libraries/noneuclid.js
+++ libraries/noneuclid.js
@@ -89753,8 +89753,8 @@ module.exports.TinyEmitter = E;
 },{}],956:[function(require,module,exports){
 const math = require('mathjs');
 
-function sine(theta, kappa = 1) {
-  return kappa == 0 ? theta : kappa > 0 ? math.sin(theta * kappa) : math.sinh(theta * kappa);
+function sine(theta, kappa = 1, s=false) {
+  return kappa == 0 ? (s?0:theta) : kappa > 0 ? math.sin(theta * kappa) : math.sinh(theta * kappa);
 }
 function cosine(theta, kappa = 1) {
   return kappa == 0 ? 1 : kappa > 0 ? math.cos(theta * kappa) : math.cosh(theta * kappa);
@@ -89764,23 +89764,23 @@ class Point {
   constructor(x, y, theta = 0, kappa=1) {
     this.kappa = kappa;
     this.mat = math.multiply(
+      math.multiply(
+        math.matrix([
+          [cosine(x, this.kappa), -sine(x, this.kappa, true), 0],
+          [sine(x, this.kappa), cosine(x, this.kappa), 0],
+          [0, 0, 1],
+        ]),
+        math.matrix([
+          [cosine(y, this.kappa), 0, -sine(y, this.kappa, true)],
+          [0, 1, 0],
+          [sine(y, this.kappa), 0, cosine(y, this.kappa)],
+        ]),
+      ),
       math.matrix([
         [1, 0, 0],
         [0, cosine(theta, 1), -sine(theta, 1)],
         [0, sine(theta, 1), cosine(theta, 1)],
       ]),
-      math.multiply(
-        math.matrix([
-          [cosine(y, this.kappa), 0, -sine(y, this.kappa)],
-          [0, 1, 0],
-          [sine(y, this.kappa), 0, cosine(y, this.kappa)],
-        ]),
-        math.matrix([
-          [cosine(x, this.kappa), -sine(x, this.kappa), 0],
-          [sine(x, this.kappa), cosine(x, this.kappa), 0],
-          [0, 0, 1],
-        ]),
-      ),
     );
   }
   get project() {
