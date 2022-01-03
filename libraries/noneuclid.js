2b760ac42bf58366e23f476f346406b1bb9ad5c0
HanchaiN <85230240+HanchaiN@users.noreply.github.com>
2022-01-03 15:22:54 +0000

    Update libraries

--- libraries/noneuclid.js
+++ libraries/noneuclid.js
@@ -89771,14 +89771,14 @@ class Point {
       ]),
       math.multiply(
         math.matrix([
-          [cosine(y, this.kappa), 0, -sine(y, this.kappa)],
-          [0, 1, 0],
-          [sine(y, this.kappa), 0, cosine(y, this.kappa)],
-        ]),
-        math.matrix([
           [cosine(x, this.kappa), -sine(x, this.kappa), 0],
           [sine(x, this.kappa), cosine(x, this.kappa), 0],
           [0, 0, 1],
+        ]),
+        math.matrix([
+          [cosine(y, this.kappa), 0, -sine(y, this.kappa)],
+          [0, 1, 0],
+          [sine(y, this.kappa), 0, cosine(y, this.kappa)],
         ]),
       ),
     );
