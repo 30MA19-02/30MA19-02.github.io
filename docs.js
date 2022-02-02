df454233d4f23f6275ed9ad422c4d12241206bf5
HanchaiN <85230240+HanchaiN@users.noreply.github.com>
2022-02-02 22:28:57 +0700

    Change `npm run build-docs`.

--- docs.js
+++ docs.js
@@ -1,9 +1,9 @@
 requirejs.config({
-    baseUrl: ".",
-    paths: {
-        "noneuclid": "lib/noneuclid",
-        "p5": "https://cdn.jsdelivr.net/npm/p5@1.4.0/lib/p5"
-    }
+  baseUrl: ".",
+  paths: {
+    p5: "https://cdn.jsdelivr.net/npm/p5@1.4.0/lib/p5",
+    noneuclid: "lib/noneuclid",
+  },
 });
 
 requirejs(["app/main"]);
