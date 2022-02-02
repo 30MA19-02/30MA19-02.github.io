0f5f67284a2021d5617fcfd91628f4b85e7ebd45
HanchaiN <85230240+HanchaiN@users.noreply.github.com>
2022-02-02 22:28:57 +0700

    Change `npm run build-docs`.

--- docs-build.js
+++ docs-build.js
@@ -1,11 +1,27 @@
-({
-    baseUrl: ".",
-    paths: {
-        requireLib: "../node_modules/requirejs/require",
-        "noneuclid": "../dist/noneuclid",
-        "p5": "../node_modules/p5/lib/p5"
-    },
-    name: "docs",
-    include: "requireLib",
-    out: "docs-built.js"
-})
+var requirejs = require("requirejs");
+
+var config = {
+  baseUrl: __dirname,
+  paths: {
+    requireLib: "../node_modules/requirejs/require",
+    p5: "../node_modules/p5/lib/p5",
+    noneuclid: "lib/noneuclid",
+  },
+  name: "docs",
+  include: "requireLib",
+  out: __dirname+"/docs-built.js",
+};
+
+requirejs.optimize(
+  config,
+  function (buildResponse) {
+    //buildResponse is just a text output of the modules
+    //included. Load the built file for the contents.
+    //Use config.out to get the optimized file contents.
+    var contents = fs.readFileSync(config.out, "utf8");
+  },
+  function (err) {
+    //optimization err callback
+    console.log(err);
+  }
+);
