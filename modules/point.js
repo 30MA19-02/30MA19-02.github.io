d8d994f4b48d8106b8bb4b90cb888ff98bf1a471
HanchaiN <85230240+HanchaiN@users.noreply.github.com>
2022-02-03 02:45:31 +0700

    Unused requirejs
    Update package.json

--- modules/point.js
+++ modules/point.js
@@ -1,4 +1,4 @@
-const {sin, sinh, cos, cosh, multiply, matrix} = require('mathjs');
+import {sin, sinh, cos, cosh, multiply, matrix} from 'mathjs';
 
 function sine(theta, kappa = 1, s = false) {
   return kappa == 0 ? (s ? 0 : theta) : kappa > 0 ? sin(theta * kappa) : (s ? -sinh(theta * kappa) : sinh(theta * kappa));
@@ -7,7 +7,7 @@ function cosine(theta, kappa = 1) {
   return kappa == 0 ? 1 : kappa > 0 ? cos(theta * kappa) : cosh(theta * kappa);
 }
 
-class Point {
+export class Point {
   constructor(x, y, theta, kappa) {
     this.kappa = kappa;
     this.mat = multiply(
@@ -18,14 +18,14 @@ class Point {
       ]),
       multiply(
         matrix([
-          [cosine(y, this.kappa), 0, -sine(y, this.kappa, true)],
-          [0, 1, 0],
-          [sine(y, this.kappa), 0, cosine(y, this.kappa)],
-        ]),
-        matrix([
           [cosine(x, this.kappa), -sine(x, this.kappa, true), 0],
           [sine(x, this.kappa), cosine(x, this.kappa), 0],
           [0, 0, 1],
+        ]),
+        matrix([
+          [cosine(y, this.kappa), 0, -sine(y, this.kappa, true)],
+          [0, 1, 0],
+          [sine(y, this.kappa), 0, cosine(y, this.kappa)],
         ]),
       ),
     );
@@ -49,7 +49,3 @@ class Point {
     return p;
   }
 }
-
-module.exports = {
-  Point: Point
-};
