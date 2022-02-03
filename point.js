ce34f29ac19fa18ec17402708be4d1eddd06bad9
HanchaiN <85230240+HanchaiN@users.noreply.github.com>
2022-02-03 17:56:33 +0700

    Try extend dimensionality.

---
+++ point.js
@@ -0,0 +1,9 @@
+import * as noneuc from "noneuclid";
+
+export class Point extends noneuc.Point {
+  constructor(lat, lon, dir, kappa) {
+    let [x, y, theta] =
+      (-lat * 2 * Math.PI, lon * 2 * Math.PI, -dir * 2 * Math.PI);
+    super([x, y], [theta], kappa);
+  }
+}
