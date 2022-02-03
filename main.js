ff1653442a62e80f753b3f8777ace1c79cb0ea11
HanchaiN <85230240+HanchaiN@users.noreply.github.com>
2022-02-03 12:24:11 +0700

    Manage HTML elements.

--- main.js
+++ main.js
@@ -8,8 +8,76 @@ import * as noneuc from "noneuclid";
 
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { ParametricGeometry } from "three/examples/jsm/geometries/ParametricGeometry";
+import { PairedSlider } from "./slider";
 
-const canvas = document.getElementById("app");
+const div = document.body.querySelector("#app");
+
+const canvas = document.createElement("canvas");
+canvas.width = 500;
+canvas.height = 500;
+
+const height_slider = new PairedSlider();
+height_slider.div.id = "height";
+height_slider.max = 32;
+height_slider.min = 2;
+height_slider.step = 1;
+height_slider.value = 32;
+
+const width_slider = new PairedSlider();
+width_slider.div.id = "width";
+width_slider.max = 24;
+width_slider.min = 3;
+width_slider.step = 1;
+width_slider.value = 24;
+
+const lat_slider = new PairedSlider();
+lat_slider.div.id = "latitude";
+lat_slider.max = +0.25;
+lat_slider.min = -0.25;
+lat_slider.step = 0;
+lat_slider.value = 0.03815754722;
+
+const lon_slider = new PairedSlider();
+lon_slider.div.id = "lontitude";
+lon_slider.max = +0.5;
+lon_slider.min = -0.5;
+lon_slider.step = 0;
+lon_slider.value = 0.27923107222;
+
+const the_slider = new PairedSlider();
+the_slider.div.id = "direction";
+the_slider.max = +0.5;
+the_slider.min = -0.5;
+the_slider.step = 0;
+the_slider.value = 0;
+
+const kappa_slider = new PairedSlider();
+kappa_slider.div.id = "kappa";
+kappa_slider.max = +1;
+kappa_slider.min = -1;
+kappa_slider.step = 0;
+kappa_slider.value = 1;
+{
+  const param = document.createElement("form");
+  const segment_label = document.createElement("label");
+  segment_label.textContent = "Mesh segments";
+  param.appendChild(segment_label);
+  param.appendChild(height_slider.div);
+  param.appendChild(width_slider.div);
+  const pos_label = document.createElement("label");
+  pos_label.textContent = "Position";
+  param.appendChild(pos_label);
+  param.appendChild(lat_slider.div);
+  param.appendChild(lon_slider.div);
+  param.appendChild(the_slider.div);
+  const kappa_label = document.createElement("label");
+  kappa_label.textContent = "Kappa";
+  param.appendChild(kappa_label);
+  param.appendChild(kappa_slider.div);
+
+  div.appendChild(canvas);
+  div.appendChild(param);
+}
 
 const scene = new THREE.Scene();
 
@@ -31,55 +99,6 @@ renderer.setSize(canvas.offsetWidth, canvas.offsetHeight);
 camera.position.setZ(30);
 
 renderer.render(scene, camera);
-
-const height_slider = document.createElement("input");
-height_slider.type = "range";
-height_slider.max = 32;
-height_slider.min = 2;
-height_slider.step = 1;
-height_slider.value = 32;
-
-const width_slider = document.createElement("input");
-width_slider.type = "range";
-width_slider.max = 24;
-width_slider.min = 3;
-width_slider.step = 1;
-width_slider.value = 24;
-
-const lat_slider = document.createElement("input");
-lat_slider.type = "range";
-lat_slider.max = +0.25;
-lat_slider.min = -0.25;
-lat_slider.step = 1e-18;
-lat_slider.value = 0.03815754722;
-
-const lon_slider = document.createElement("input");
-lon_slider.type = "range";
-lon_slider.max = +0.5;
-lon_slider.min = -0.5;
-lon_slider.step = 1e-18;
-lon_slider.value = 0.27923107222;
-
-const the_slider = document.createElement("input");
-the_slider.type = "range";
-the_slider.max = +0.5;
-the_slider.min = -0.5;
-the_slider.step = 1e-18;
-the_slider.value = 0;
-
-const kappa_slider = document.createElement("input");
-kappa_slider.type = "range";
-kappa_slider.max = +1;
-kappa_slider.min = -1;
-kappa_slider.step = 1e-18;
-kappa_slider.value = 0;
-
-document.body.appendChild(height_slider);
-document.body.appendChild(width_slider);
-document.body.appendChild(lat_slider);
-document.body.appendChild(lon_slider);
-document.body.appendChild(the_slider);
-document.body.appendChild(kappa_slider);
 
 const texture = new THREE.TextureLoader().load(textureUrl);
 const textured_material = new THREE.MeshBasicMaterial({
---
+++ slider.js
@@ -0,0 +1,58 @@
+export class PairedSlider{
+    constructor(){
+        this.slider = document.createElement("input");
+        this.slider.type = "range";
+        this.slider.name = "slider";
+        this.number = document.createElement("input");
+        this.number.type = "number";
+        this.number.name = "number";
+        this.div = document.createElement("div");
+        this.div.appendChild(this.slider);
+        this.div.appendChild(this.number);
+        let pairedSlider = this;
+        this.slider.onchange = function(){
+           pairedSlider.value =pairedSlider.slider.value;
+        }
+        this.number.onchange = function(){
+           pairedSlider.value = pairedSlider.number.value;
+        }
+    }
+    get value(){
+        return this.slider.value;
+    }
+    set value(value){
+        this.slider.value = value;
+        this.number.value = this.value;
+    }
+    get max(){
+        return this.slider.max;
+    }
+    set max(value){
+        this.slider.max = value;
+        this.number.max = this.max;
+    }
+    get min(){
+        return this.slider.min;
+    }
+    set min(value){
+        this.slider.min = value;
+        this.number.min = this.min;
+    }
+    get step(){
+        return this.slider.step;
+    }
+    set step(value){
+        if (value!=0) {
+            this.slider.step = value;
+            this.number.step = this.step;
+        } else {
+            this.slider.step = 1e-18;
+        }
+    }
+    setAttribute(qualifiedName, value){
+        this.slider.setAttribute(qualifiedName, value);
+    }
+    getAttribute(qualifiedName){
+        this.slider.getAttribute(qualifiedName);
+    }
+}
