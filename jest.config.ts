30b1a79eeb8689ee0bfd505f4f7151802c626e94
HanchaiN <85230240+HanchaiN@users.noreply.github.com>
2022-02-11 21:58:06 +0700

    Update jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -152,7 +152,7 @@ const config: Config.InitialOptions = {
   // ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
-  testRegex: ['text/**/*.test.ts'],
+  // testRegex: [],
 
   // This option allows the use of a custom results processor
   // testResultsProcessor: undefined,
