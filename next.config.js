f043e187da90dec42df8597b92f47739e541d5a2
HanchaiN <85230240+HanchaiN@users.noreply.github.com>
2022-04-27 19:57:28 +0700

    Rename pages

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
       ...defaultPathMap,
     };
   },
-  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
   images: {
     loader: 'imgix',
     path: '',
